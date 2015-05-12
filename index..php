1a400b4c57d6702ac13d74dc88f017ecc86425bb
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-12 08:55:32 -0700

    commit 1

---
+++ index..php
@@ -0,0 +1,28 @@
+<?php 
+//configuration for our php Server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//make constant using define
+define{'client_ID', '82db6747d1fd4a97950ce2329c23299f'};
+define{'client_Secret', '310890515b3c4eed8aaeabd40dafdbf6'};
+define{'redirectURL', 'http://localhost:8888/instagram-api/index.php'};
+define{'ImageDirectory', 'pics/'};
+?>
+
+<!-- CLIENT INFO
+CLIENT ID	82db6747d1fd4a97950ce2329c23299f
+CLIENT SECRET	310890515b3c4eed8aaeabd40dafdbf6
+WEBSITE URL	http://localhost:8888/instagram-api/index.php
+REDIRECT URI	http://localhost:8888/instagram-api/index.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
