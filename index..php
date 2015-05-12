3e4ce0ec749ab1e6a5efaf1b6f30a5e8c1236288
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-12 09:15:40 -0700

    commit 2 test

--- index..php
+++ index..php
@@ -5,9 +5,9 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //make constant using define
-define{'client_ID', '82db6747d1fd4a97950ce2329c23299f'};
+define{'clientID', '82db6747d1fd4a97950ce2329c23299f'};
 define{'client_Secret', '310890515b3c4eed8aaeabd40dafdbf6'};
-define{'redirectURL', 'http://localhost:8888/instagram-api/index.php'};
+define{'redirectURL', 'http://localhost/instagram-api/index.php'};
 define{'ImageDirectory', 'pics/'};
 ?>
 
@@ -20,9 +20,16 @@ REDIRECT URI	http://localhost:8888/instagram-api/index.php -->
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Untitled</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="author" href="humans.txt">
 </head>
 <body>
-
+<!-- Creating a lgoin for people to go and give approval for our web app to access their Instagram account after getting approval we are now going to hae the information so that we can play with it. -->
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURL;?>&response_type=code">LOGIN</a>
+	<script src="js/main.js"></script>
 </body>
 </html>
